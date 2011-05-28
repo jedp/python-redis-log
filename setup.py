6e21b564a9d316dc9f4df25ad8d9bbd6a7977e5a
Jed Parsons <jedp@me.com>
2011-05-27 22:27:52 -0700

    python build/install script

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
+    
+setup(
+      name='python-redis-log',
+      version='0.0.1',
+      description='Redis pub/sub logging handler for python',
+      author='Jed Parsons',
+      author_email='jed@jedparsons.com',
+      url='git://github.com/jedp/python-redis-log.git',
+      packages=['python-redis-log'],
+      install_requires=['redis', 'simplejson']
+)
