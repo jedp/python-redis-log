dcfa4eff0ece9e2a5913df53c6c68f259940643d
Jed Parsons <jedp@me.com>
2011-05-29 22:35:51 -0700

    added data_files: README.md and LICENSE

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ except ImportError:
     
 setup(
       name='python-redis-log',
-      version='0.0.1',
+      version='0.1.0',
       description='Redis pub/sub logging handler for python',
       author='Jed Parsons',
       author_email='jed@jedparsons.com',
       url='git://github.com/jedp/python-redis-log.git',
       packages=['redislog'],
+      data_files=[('', ['LICENSE', 'README.md'])],
+      license='MIT',
       install_requires=['redis', 'simplejson']
 )
