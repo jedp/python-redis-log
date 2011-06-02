ea9e12b320eb6ed7bea3523cb058c1e8fb041131
Jed Parsons <jedp@me.com>
2011-06-02 08:17:21 -0700

    README.rst is now long description on pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from os import path
+
+README = path.abspath(path.join(path.dirname(__file__), 'README.rst'))
+
 setup(
       name='python-redis-log',
-      version='0.1.1',
+      version='0.1.2',
       description='Redis pub/sub logging handler for python',
+      long_description=open(README).read(),
       author='Jed Parsons',
       author_email='jed@jedparsons.com',
       url='https://github.com/jedp/python-redis-log',
