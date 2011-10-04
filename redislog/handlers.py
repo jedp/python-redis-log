ab020ae4e2b7d69852d18c04e1f556c6da828bcb
Peter Vessenes <vessenes@gmail.com>
2011-10-04 10:02:43 -0700

    Add password support to redis. Also, fix a bunch of pep8 problems

--- redislog/handlers.py
+++ redislog/handlers.py
@@ -2,6 +2,7 @@ import logging
 import redis
 import simplejson as json
 
+
 class RedisFormatter(logging.Formatter):
     def format(self, record):
         """
@@ -17,20 +18,21 @@ class RedisFormatter(logging.Formatter):
         # stringify exception data
         if data.get('traceback'):
             data['traceback'] = self.formatException(data['traceback'])
-    
+
         return json.dumps(data)
+
 
 class RedisHandler(logging.Handler):
     """
     Publish messages to redis channel.
 
-    As a convenience, the classmethod to() can be used as a 
+    As a convenience, the classmethod to() can be used as a
     constructor, just as in Andrei Savu's mongodb-log handler.
     """
 
     @classmethod
-    def to(cklass, channel, host='localhost', port=6379, level=logging.NOTSET):
-        return cklass(channel, redis.Redis(host=host, port=port), level=level)
+    def to(cklass, channel, host='localhost', port=6379, password=None, level=logging.NOTSET):
+        return cklass(channel, redis.Redis(host=host, port=port, password=password), level=level)
 
     def __init__(self, channel, redis_client, level=logging.NOTSET):
         """
@@ -45,11 +47,12 @@ class RedisHandler(logging.Handler):
         """
         Publish record to redis logging channel
         """
-        try :
+        try:
             self.redis_client.publish(self.channel, self.format(record))
         except redis.RedisError:
             pass
- 
+
+
 class RedisListHandler(logging.Handler):
     """
     Publish messages to redis a redis list.
@@ -78,13 +81,13 @@ class RedisListHandler(logging.Handler):
         """
         Publish record to redis logging list
         """
-        try :
+        try:
             if self.max_messages:
                 p = self.redis_client.pipeline()
                 p.rpush(self.key, self.format(record))
                 p.ltrim(self.key, -self.max_messages, -1)
                 p.execute()
             else:
-                self.redis_client.rpush(self.key,self.format(record))
+                self.redis_client.rpush(self.key, self.format(record))
         except redis.RedisError:
             pass
